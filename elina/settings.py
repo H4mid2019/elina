dab73009e9c05e285bd35808128ff9ed5bfbf61e
H4mid2019 <hamidreza.darabi@hotmail.com>
2022-01-11 01:20:45 +0200

    secret key and debug removed from settings

--- elina/settings.py
+++ elina/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,15 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8khne-%95$ina%b!1%@5au2js1&vx5gz0q@j2za9if4p5v18a)'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-8khne-%95$ina%b!1%@5au2js1&vx5gz0q@j2za9if4p5v18a)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
-CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+
 # Application definition
 
 INSTALLED_APPS = [
