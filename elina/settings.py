eea40279994ea3230009f1a809d05622e9257307
H4mid2019 <hamidreza.darabi@hotmail.com>
2022-02-20 18:39:26 +0200

    change enviroment names

--- elina/settings.py
+++ elina/settings.py
@@ -89,9 +89,9 @@ WSGI_APPLICATION = 'elina.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('POSTGRES_HOST'),
         'PORT': '5432',
     }
